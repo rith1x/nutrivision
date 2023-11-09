69b109dd7dce4e742e03c57a40b92de9c6804a79
rith1x <kiruthikkumarmpc24@gmail.com>
2023-11-09 20:58:03 +0530

    Updated Version

--- cpanel/views.py
+++ cpanel/views.py
@@ -30,49 +30,86 @@ def signIn(request):
 global name
 
 
+# def postSignIn(request):
+#     global local_id
+
+#     email = request.POST.get("email")
+#     passw = request.POST.get("pass")
+
+#     try:
+#         user = auth.sign_in_with_email_and_password(email, passw)
+#         idtoken = user["idToken"]
+
+#         # Get the user's email verification status
+#         user_info = auth.get_account_info(idtoken)
+#         email_verified = user_info["users"][0]["emailVerified"]
+
+#         if not email_verified:
+#             message = (
+#                 "Email not verified. Please check your inbox and verify your email."
+#             )
+#             return render(request, "signIn.html", {"messg": message})
+
+
+#         # Access the localId
+#         local_id = user_info["users"][0]["localId"]
+#         msg = "Login Successful!"
+#         # You can use the 'local_id' to access the Firebase Realtime Database
+#         return welcomeLoader(request, msg)
+#     except Exception as e:
+#         message = "Invalid credentials: " + str(e)
+#         return render(request, "signIn.html", {"messg": message})
 def postSignIn(request):
     global local_id
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        print(email, password)
 
-    email = request.POST.get("email")
-    passw = request.POST.get("pass")
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            print(user)
+            if user is not None and "idToken" in user:
+                print("entered if")
+                idtoken = user["idToken"]
+                user_info = auth.get_account_info(idtoken)
+                local_id = user_info["users"][0]["localId"]
+                msg = "Login Successful"
+                return welcomeLoader(request, msg)
+            else:
+                print("else enter")
+                message = "Invalid credentials: User not found"
+                return render(request, "signIn.html", {"messg": message})
 
-    try:
-        user = auth.sign_in_with_email_and_password(email, passw)
-        idtoken = user["idToken"]
-
-        # Get the user's email verification status
-        user_info = auth.get_account_info(idtoken)
-        email_verified = user_info["users"][0]["emailVerified"]
-
-        if not email_verified:
-            message = (
-                "Email not verified. Please check your inbox and verify your email."
-            )
+        except Exception as e:
+            message = "Invalid credentials: " + str(e)
             return render(request, "signIn.html", {"messg": message})
 
-        # Access the localId
-        local_id = user_info["users"][0]["localId"]
-        msg = "Login Successful!"
-        # You can use the 'local_id' to access the Firebase Realtime Database
-        return welcomeLoader(request, msg)
-    except Exception as e:
-        message = "Invalid credentials: " + str(e)
-        return render(request, "signIn.html", {"messg": message})
+    return render(request, "signIn.html")
 
 
 def welcomeLoader(request, msg):
+    global local_id
+    print(local_id)
     db = firebase.database()
+    print(db)
     global name
+
     name = db.child("users").child(local_id).child("details").child("name").get().val()
+    print(name)
     user_path = f"users/{local_id}/details/status"
+    print(user_path)
     db.child(user_path).set("1")
     user_data_path = f"users/{local_id}"
     caloriedata = db.child(user_data_path + "/calorieData").get().val()
     bmidata = db.child(user_data_path + "/bmiData").get().val()
+    # if caloriedata and bmidata:
+    # print("if kulla irken")
+    ncpd = int(db.child(user_data_path + "/details/ncpd").get().val())
     calorie_data = [
         {"date": key, "calorie": value} for key, value in caloriedata.items()
     ]
-    bmi_data = [{"date": key, "calorie": value} for key, value in bmidata.items()]
+    bmi_data = [{"date": key, "bmi": value} for key, value in bmidata.items()]
     return render(
         request,
         "welcome.html",
@@ -81,17 +118,34 @@ def welcomeLoader(request, msg):
             "user": local_id,
             "bmiData": bmi_data,
             "calorieData": calorie_data,
+            "ncpd": ncpd,
             "mssg": msg,
         },
     )
+    # else:
+    # calorie_data = []
+    # bmi_data = []
+    # print("else kulla")
+    # return render(
+    # request,
+    # "welcome.html",
+    # {
+    # "email": name,
+    # "user": local_id,
+    # "bmiData": bmi_data,
+    # "calorieData": calorie_data,
+    # "mssg": msg,
+    # },
+    # )
 
 
 def logout(request):
-    auth.current_user = None
+    global local_id
     db = firebase.database()
     user_path = f"users/{local_id}/details/status"
 
     db.child(user_path).set("0")
+    auth.current_user = None
 
     return render(request, "signIn.html")
 
@@ -101,25 +155,74 @@ def signUp(request):
 
 
 def postSignUp(request):
+    global local_id
     name = request.POST.get("name")
     email = request.POST.get("email")
     passw = request.POST.get("pass")
+    height = request.POST.get("height")
+    weight = request.POST.get("weight")
+    age = request.POST.get("age")
+    sex = request.POST.get("sex")
+    print(name)
+    print(email)
+    print(passw)
+    print(height)
+    print(weight)
     try:
         user = auth.create_user_with_email_and_password(email, passw)
         auth.send_email_verification(user["idToken"])
+        print(user)
+        print(" in try")
     except Exception as e:
+        print("in ex")
         message = "Unable to create account. Try again: " + str(e)
         return render(request, "signUp.html", {"messg": message})
 
     uid = user["localId"]
-
-    data = {"name": name, "status": "1"}
-    mssg = "A verification email has been sent to your email address. Please check your inbox and verify your email before signing in."
+    local_id = uid
+    print(uid)
+    data = {
+        "name": name,
+        "status": "1",
+        "email": email,
+        "height": height,
+        "weight": weight,
+        "age": age,
+        "sex": sex,
+    }
+    print(data)
     database.child("users").child(uid).child("details").set(data)
-    return render(request, "signIn.html", {"messg": mssg})
+    ncpdd = calculate_calorie(height, weight, age, sex)
+    bmi = calculate_bmi(height, weight)
+    msg = ncpdd + "Signup Successfull"
+    return render(request, "signIn.html", {"messg": msg})
 
 
 # ----------------------- Authentication ENDS ---------------------------
+
+
+def calculate_bmi(h, w):
+    global local_id
+    db = firebase.database()
+
+    hm = float(h) / 100
+    wk = float(w) / 100
+    hs = hm * hm
+    bmx = wk / hs
+    bmi = round(bmx, 2)
+    bmi_path = f"users/{local_id}/details"
+    db.child(bmi_path + "/bmi").set(bmi)
+    if bmi < 18.5:
+        category = "Underweight"
+    elif 18.5 <= bmi < 24.9:
+        category = "Normal Weight"
+    elif 25 <= bmi < 29.9:
+        category = "Overweight"
+    else:
+        category = "Obese"
+    db.child(bmi_path + "/bmi_category").set(category)
+
+    return bmi
 
 
 def create(request):
@@ -314,12 +417,10 @@ from tensorflow.keras.preprocessing import image
 
 
 # Load your trained Keras model
-model = tf.keras.models.load_model(
-    "C:\\Users\\Kiruthik Kumar R\\Desktop\\Nutrivision\\models\\keras_model.h5"
-)
+model = tf.keras.models.load_model("models\\keras_modelv3.h5")
 
 # Load the class labels
-with open("C:\\Users\\Kiruthik Kumar R\\Desktop\\Nutrivision\\models\\labels.txt") as f:
+with open("models\\labels4.txt") as f:
     labels = f.readlines()
 labels = [label.strip() for label in labels]
 
@@ -330,7 +431,7 @@ labels = [label.strip() for label in labels]
 
 # Function to classify an image
 def classify_image(file_url):
-    img = image.load_img(file_url, target_size=(224, 224))
+    img = image.load_img(file_url, target_size=(224, 224, 3))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     img = img / 255.0  # Normalize
@@ -346,20 +447,10 @@ def scanner(request):
 
     if request.method == "POST":
         cap = cv2.VideoCapture(0)
-
-        # Read a frame from the webcam
         ret, frame = cap.read()
-
-        # Define a file name for the captured image
         file_name = "static/captured_image.jpg"
-
-        # Save the frame as an image
         cv2.imwrite(file_name, frame)
-
-        # Release the webcam
         cap.release()
-
-        # Classify the captured image
         Label = classify_image(file_name)
 
         api_url = "https://api.calorieninjas.com/v1/nutrition?query="
@@ -385,12 +476,89 @@ def addCal(request):
     global local_id
 
     calorie = float(request.POST["calorie"])
+    cal_food = float(request.POST["calFood"])
     count = int(request.POST["count"])
-    totalCalorie = count * calorie
-    calo = round(totalCalorie, 2)
 
+    final_cal = (cal_food) * (count / 100)
+    carbos = float(request.POST["carbos"]) * (count / 100)
+    choles = float(request.POST["choles"]) * (count / 100)
+    satfat = float(request.POST["satfat"]) * (count / 100)
+    totfat = float(request.POST["totfat"]) * (count / 100)
+    fibres = float(request.POST["fibres"]) * (count / 100)
+    potass = float(request.POST["potass"]) * (count / 100)
+    protei = float(request.POST["protein"]) * (count / 100)
+    sodium = float(request.POST["sodium"]) * (count / 100)
+    sugars = float(request.POST["sugars"]) * (count / 100)
+
+    calo = round(final_cal, 2)
+
+    nte = nutriAdd(
+        carbos, choles, satfat, totfat, fibres, potass, protei, sodium, sugars
+    )
     msg = dbcalAdd(calo)
     return welcomeLoader(request, msg)
+
+
+def nutriAdd(carbos, choles, satfat, totfat, fibres, potass, protei, sodium, sugars):
+    global local_id
+    from datetime import datetime
+
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    db = firebase.database()
+    nutripath = f"users/{local_id}/nutrientIntake"
+    carb = db.child(nutripath).get().val()
+
+    if carb is None:
+        carbos_e = 0
+        choles_e = 0
+        satfat_e = 0
+        totfat_e = 0
+        fibres_e = 0
+        potass_e = 0
+        protei_e = 0
+        sodium_e = 0
+        sugars_e = 0
+    else:
+        carbos_e = float(db.child(nutripath + "/carbos").get().val())
+        choles_e = float(db.child(nutripath + "/choles").get().val())
+        satfat_e = float(db.child(nutripath + "/satfat").get().val())
+        totfat_e = float(db.child(nutripath + "/totfat").get().val())
+        fibres_e = float(db.child(nutripath + "/fibres").get().val())
+        potass_e = float(db.child(nutripath + "/potass").get().val())
+        protei_e = float(db.child(nutripath + "/protei").get().val())
+        sodium_e = float(db.child(nutripath + "/sodium").get().val())
+        sugars_e = float(db.child(nutripath + "/sugars").get().val())
+    carbos_u = carbos_e + carbos
+    choles_u = choles_e + ((choles / 1000) * 300)
+    satfat_u = satfat_e + satfat
+    totfat_u = totfat_e + totfat
+    fibres_u = fibres_e + fibres
+    potass_u = potass_e + potass
+    protei_u = protei_e + protei
+    sodium_u = sodium_e + ((sodium / 1000) * 300)
+    sugars_u = sugars_e + ((sugars / 1000) * 300)
+
+    carbos_r = round(carbos_u, 2)
+    choles_r = round(choles_u, 2)
+    satfat_r = round(satfat_u, 2)
+    totfat_r = round(totfat_u, 2)
+    fibres_r = round(fibres_u, 2)
+    potass_r = round(potass_u, 2)
+    protei_r = round(protei_u, 2)
+    sodium_r = round(sodium_u, 2)
+    sugars_r = round(sugars_u, 2)
+
+    db.child(nutripath + "/carbos").set(carbos_r)
+    db.child(nutripath + "/choles").set(choles_r)
+    db.child(nutripath + "/satfat").set(satfat_r)
+    db.child(nutripath + "/totfat").set(totfat_r)
+    db.child(nutripath + "/fibres").set(fibres_r)
+    db.child(nutripath + "/potass").set(potass_r)
+    db.child(nutripath + "/protei").set(protei_r)
+    db.child(nutripath + "/sodium").set(sodium_r)
+    db.child(nutripath + "/sugars").set(sugars_r)
+
+    return "Nutri Data Added Successfully!"
 
 
 def dbcalAdd(cals):
@@ -411,3 +579,30 @@ def dbcalAdd(cals):
     # Update the database with the updated data
     db.child(calorie_data_path).set(updated_calorie_data)
     return "Calorie Data Added Successfully!"
+
+
+def calculate_calorie(height, weight, age, sex):
+    global local_id
+    db = firebase.database()
+    # pathx = f"users/{local_id}/details"
+
+    # weight = float(db.child(pathx / weight).get().val())
+    # height = float(db.child(pathx / height).get().val())
+    # sex = db.child(pathx / sex).get().val()
+    # age = int(db.child(pathx / age).get().val())
+
+    w = float(weight)
+    h = float(height)
+    a = int(age)
+    s = str(sex)
+
+    if s == "male":
+        bmr = 88.362 + (13.397 * w) + (4.799 * h) - (5.677 * a)
+    else:
+        bmr = 447.593 + (9.247 * w) + (3.098 * h) - (4.330 * a)
+    tdee = bmr * 1.55
+    ncpd = round(tdee, 2)
+    ncpd_path = f"users/{local_id}/details/ncpd"
+    db.child(ncpd_path).set(ncpd)
+    msg = "NCPD UPDATED, "
+    return msg
